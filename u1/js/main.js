536620e623c7e3f413ba0dec8253d037f8a941a4
sannda <46871341+sannda@users.noreply.github.com>
2019-09-29 21:20:08 +0400

    Add files via upload

---
+++ u1/js/main.js
@@ -0,0 +1,21 @@
+"use strict";
+// Unit 1. Стартуем и пишем первую программу
+
+console.log('Орская Оксана'); // string
+console.log(11); // number
+console.log('Добро' + ' пожаловать' + ' на курс');
+console.log(2019-200);
+//
+let a = 12 *2;
+document.getElementById('one').innerHTML = 'Hello, Oksana Orskaya';
+document.getElementById('two').innerHTML = a;
+document.querySelector('span').innerHTML = 'world';
+document.querySelector('.three').innerHTML = '<h3>Header</h3>';
+document.querySelector('.four').innerHTML +='<h4>Hello, Oksana</h4> <p>My name Oksana Orskaya</p>';
+let y1 = 6;
+let y2 = 3;
+document.querySelector('.five').innerHTML = y1 / y2;
+let d2 = document.querySelector('.test-2');
+console.log(d2);
+d2 =5;
+console.log(d2);
