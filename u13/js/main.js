82bcfb2a5974a8632627132e9cccb942173f0b90
sannda <46871341+sannda@users.noreply.github.com>
2019-11-04 22:30:04 +0400

    Add files via upload

--- u13/js/main.js
+++ u13/js/main.js
@@ -1,3 +1,18 @@
 // Unit 13. Ассоциативный массив (объект) в JavaScript
 // Practic
 "use struct"
+let a = {
+  "fam": 'Дриголя',
+  "name": 'Оксана',
+  "age": 28,
+  "breast": 2,
+
+};
+// записать в массив
+a.city = "Belgorod";
+a.fam = 'Орская';
+console.log(a);
+console.log(a.name);
+let k = "age";
+console.log(a[k]);
+
